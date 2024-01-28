7e7dea2d35b0d9eb9d1a97c6b44c47cb476c9610
Amir Solati <amirsolati55@gmail.com>
2024-01-28 17:40:13 +0330

    add other section of index

--- pages/index.vue
+++ pages/index.vue
@@ -450,7 +450,7 @@
     </div>
     <div class="grid grid-cols-12">
       <div class="col-span-12 flex justify-center mt-16">
-        <NuxtLink to="/" class="bg-thred text-thwhite font-light font-xl px-16 py-6 flex flex-row content-center">
+        <NuxtLink to="/" class="bg-thred text-thwhite min-w-48 font-light font-xl px-16 py-6 flex flex-row content-center">
           <svg class="mt-1 mr-2" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
             <path d="M9.6 16a.8.8 0 0 1-.8-.8V9.6a.8.8 0 0 1 .8-.8h5.6a.8.8 0 0 1 .8.8v5.6a.8.8 0 0 1-.8.8zm.8-1.6h4v-4h-4zM.8 16a.8.8 0 0 1-.8-.8V9.6a.8.8 0 0 1 .8-.8h5.6a.8.8 0 0 1 .8.8v5.6a.8.8 0 0 1-.8.8zm.8-1.6h4v-4h-4zm8-7.2a.8.8 0 0 1-.8-.8V.8a.8.8 0 0 1 .8-.8h5.6a.8.8 0 0 1 .8.8v5.6a.8.8 0 0 1-.8.8zm.8-1.6h4v-4h-4zM.8 7.2a.8.8 0 0 1-.8-.8V.8A.8.8 0 0 1 .8 0h5.6a.8.8 0 0 1 .8.8v5.6a.8.8 0 0 1-.8.8zm.8-1.6h4v-4h-4z" style="fill:#fff"/>
           </svg>
@@ -459,5 +459,217 @@
       </div>
     </div>
   </section>
-
+  <section class="testimonials mt-72">
+    <div class="grid grid-cols-12">
+      <div class="col-span-12 md:col-span-6 flex justify-center md:justify-start">
+        <h1 class="text-thblack-100 text-2xl">Testimonials</h1>
+      </div>
+      <div class="hidden md:flex md:col-span-6 justify-end">
+        <NuxtLink to="/" class="text-thred text-2xl">
+          See All
+        </NuxtLink>
+      </div>
+    </div>
+    <div class="grid grid-cols-12 gap-x-14 mt-14">
+      <div class="col-span-3 hidden md:block">
+          <NuxtImg src="images/testimonials.webp" />
+          <div class="bg-thred px-6 py-7">
+            <h1 class="text-thwhite font-bold text-base">
+              Peter Azarbad
+            </h1>
+            <p class="text-thwhite text-base">
+              Justin
+            </p>
+          </div>
+      </div>
+      <div class="col-span-12 md:col-span-9">
+        <div class="flex flex-row">
+          <div class="col-span-2 hidden md:block mx-3">
+            <NuxtImg src="images/testimonials.webp" />
+            <div class="bg-thgray-200 px-6 py-7">
+              <h1 class="text-thblack-200 font-semibold text-base">
+                Peter Azarbad
+              </h1>
+              <p class="text-thblack-200 opacity-40 text-base">
+                Justin
+              </p>
+            </div>
+          </div>
+          <div class="col-span-2 hidden md:block mx-3">
+            <NuxtImg src="images/testimonials.webp" />
+            <div class="bg-thred px-6 py-7">
+              <h1 class="text-thwhite font-bold text-base">
+                Peter Azarbad
+              </h1>
+              <p class="text-thwhite text-base">
+                Justin
+              </p>
+            </div>
+          </div>
+          <div class="col-span-2 hidden md:block mx-3">
+            <NuxtImg src="images/testimonials.webp" />
+            <div class="bg-thred px-6 py-7">
+              <h1 class="text-thwhite font-bold text-base">
+                Peter Azarbad
+              </h1>
+              <p class="text-thwhite text-base">
+                Justin
+              </p>
+            </div>
+          </div>
+          <div class="col-span-2 hidden md:block mx-3">
+            <NuxtImg src="images/testimonials.webp" />
+            <div class="bg-thred px-6 py-7">
+              <h1 class="text-thwhite font-bold text-base">
+                Peter Azarbad
+              </h1>
+              <p class="text-thwhite text-base">
+                Justin
+              </p>
+            </div>
+          </div>
+          <div class="col-span-2 hidden md:block mx-3">
+            <NuxtImg src="images/testimonials.webp" />
+            <div class="bg-thred px-6 py-7">
+              <h1 class="text-thwhite font-bold text-base">
+                Peter Azarbad
+              </h1>
+              <p class="text-thwhite text-base">
+                Justin
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-row mx-3 mt-11">
+          <button class="bg-thgray-200 rounded-full rotate-180 w-16 h-16 flex justify-center items-center mx-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="8.992" height="14.388" viewBox="0 0 8.992 14.388">
+              <path data-name="Path 29473" d="M172.153 128.222a.9.9 0 1 0-1.184 1.354l6.42 5.617-6.421 5.618a.9.9 0 1 0 1.184 1.354l7.194-6.295a.9.9 0 0 0 0-1.354zm0 0" transform="translate(-170.661 -128)" style="fill:#db0021"/>
+            </svg>
+          </button>
+          <button class="bg-thgray-200 rounded-full w-16 h-16 flex justify-center items-center mx-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="17" viewBox="0 0 15 17">
+              <path data-name="Polygon 4" d="M7.63 1.535a1 1 0 0 1 1.74 0l6.784 11.972a1 1 0 0 1-.87 1.493H1.716a1 1 0 0 1-.87-1.493z" transform="rotate(90 7.5 7.5)" style="fill:#db0021"/>
+            </svg>
+          </button>
+          <button class="bg-thgray-200 rounded-full w-16 h-16 flex justify-center items-center mx-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="8.992" height="14.388" viewBox="0 0 8.992 14.388">
+              <path data-name="Path 29473" d="M172.153 128.222a.9.9 0 1 0-1.184 1.354l6.42 5.617-6.421 5.618a.9.9 0 1 0 1.184 1.354l7.194-6.295a.9.9 0 0 0 0-1.354zm0 0" transform="translate(-170.661 -128)" style="fill:#db0021"/>
+            </svg>
+          </button>
+          <div ref="wavePlayer" class="flex-grow">
+            wavecontainer
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="partners mt-72">
+    <div class="grid grids-col-12 place-items-center">
+        <h1 class="text-thblack-100 text-2xl">
+            Our Partners
+        </h1>
+    </div>
+    <div class="grid grid-cols-7 gap-x-10 gap-y-24 mt-24">
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+      <div class="flex justify-center items-center">
+        <NuxtImg class="max-w-24 max-h-24" src="images/partners.png" />
+      </div>
+    </div>
+    <div class="grid grid-cols-12 mt-24">
+      <div class="col-span-12 flex justify-center">
+        <NuxtLink to="/" class="bg-thred text-thwhite font-light font-xl min-w-48 px-20 py-6 flex flex-row content-center">
+          <svg class="mt-1 mr-2" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+            <path d="M9.6 16a.8.8 0 0 1-.8-.8V9.6a.8.8 0 0 1 .8-.8h5.6a.8.8 0 0 1 .8.8v5.6a.8.8 0 0 1-.8.8zm.8-1.6h4v-4h-4zM.8 16a.8.8 0 0 1-.8-.8V9.6a.8.8 0 0 1 .8-.8h5.6a.8.8 0 0 1 .8.8v5.6a.8.8 0 0 1-.8.8zm.8-1.6h4v-4h-4zm8-7.2a.8.8 0 0 1-.8-.8V.8a.8.8 0 0 1 .8-.8h5.6a.8.8 0 0 1 .8.8v5.6a.8.8 0 0 1-.8.8zm.8-1.6h4v-4h-4zM.8 7.2a.8.8 0 0 1-.8-.8V.8A.8.8 0 0 1 .8 0h5.6a.8.8 0 0 1 .8.8v5.6a.8.8 0 0 1-.8.8zm.8-1.6h4v-4h-4z" style="fill:#fff"/>
+          </svg>
+          <p>MORE</p>
+        </NuxtLink>
+      </div>
+    </div>
+  </section>
+  <section class="callAction mt-80">
+    <div class="grid grid-cols-12 gap-24">
+      <div class="col-span-5">
+        <NuxtImg src="images/callAction.webp" />
+      </div>
+      <div class="col-span-7">
+        <svg data-name="Quote Pattern" xmlns="http://www.w3.org/2000/svg" width="70.534" height="49.791" viewBox="0 0 70.534 49.791">
+          <path d="M15.642 0a15.643 15.643 0 0 0-.907 31.259c.142 1.534.035 5.709-3.965 11.516a1.1 1.1 0 0 0 .128 1.4c1.637 1.637 2.649 2.668 3.357 3.389.927.943 1.35 1.373 1.969 1.935a1.1 1.1 0 0 0 1.464.016 42.24 42.24 0 0 0 13.594-33.953C30.625 6.545 24.048 0 15.642 0z" transform="rotate(180 35.267 24.896)" style="fill:#e60018"/>
+          <path data-name="Shape" d="M31.284 15.562C30.625 6.545 24.047 0 15.643 0a15.643 15.643 0 0 0-.906 31.259c.142 1.533.034 5.705-3.967 11.516a1.1 1.1 0 0 0 .128 1.4c1.631 1.631 2.64 2.66 3.348 3.38.931.949 1.356 1.382 1.978 1.946a1.1 1.1 0 0 0 1.464.014 42.243 42.243 0 0 0 13.596-33.953z" transform="rotate(180 15.696 24.895)" style="fill:#e60018"/>
+        </svg>
+        <h2 class="text-thblack-200 font-bold text-xl mt-10">
+          Gregory Hayes - CEO of photoin.id
+        </h2>
+        <p class="text-thblack-100 text-xl mt-2">
+          “ Double Square did excellent work on photoin.id. The project was a great success. They not only deliver innovative web and mobile applications but also are very committed to deep understand the needs of their clients. “
+        </p>
+        <div class="flex">
+          <NuxtLink to="/" class="bg-thred text-thwhite font-light font-xl min-w-48 px-20 py-6 flex flex-row content-center mt-10">
+            <svg class="mt-1 mr-2" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+              <path d="M9.6 16a.8.8 0 0 1-.8-.8V9.6a.8.8 0 0 1 .8-.8h5.6a.8.8 0 0 1 .8.8v5.6a.8.8 0 0 1-.8.8zm.8-1.6h4v-4h-4zM.8 16a.8.8 0 0 1-.8-.8V9.6a.8.8 0 0 1 .8-.8h5.6a.8.8 0 0 1 .8.8v5.6a.8.8 0 0 1-.8.8zm.8-1.6h4v-4h-4zm8-7.2a.8.8 0 0 1-.8-.8V.8a.8.8 0 0 1 .8-.8h5.6a.8.8 0 0 1 .8.8v5.6a.8.8 0 0 1-.8.8zm.8-1.6h4v-4h-4zM.8 7.2a.8.8 0 0 1-.8-.8V.8A.8.8 0 0 1 .8 0h5.6a.8.8 0 0 1 .8.8v5.6a.8.8 0 0 1-.8.8zm.8-1.6h4v-4h-4z" style="fill:#fff"/>
+            </svg>
+            <span>MORE</span>
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
